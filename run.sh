705e9767396d521d9677998a815d545dddabce77
Alexander Fefelov <alexanderfefelov@yandex.ru>
2017-01-26 05:32:12 +0300

    Use named volume

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker run --name postgrespro-1c \
   --net host \
   --detach \
   --publish 5432:5432 \
+  --volume postgrespro-1c-data:/data \
   --volume /etc/localtime:/etc/localtime:ro \
   --env POSTGRES_PASSWORD=password \
   alexanderfefelov/postgrespro-1c
